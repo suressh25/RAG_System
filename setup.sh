93ab0859ae06956dbdbf88b50ce1665618304b3a
Suresh <suresh25off@gmail.com>
2024-12-11 15:02:16 +0000

    commit

--- setup.sh
+++
@@ -1,9 +0,0 @@
-```sh
-#!/bin/bash
-
-# Update package list and install system dependencies
-sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx
-
-# Install Python dependencies
-pip install -r requirements.txt
